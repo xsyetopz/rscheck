4ced639d4a2271ed719c5e2bb1248e90ffdf348e
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-04-24 03:12:11 +0300

    remove old

--- crates/rscheck-cli/src/policy.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::config::{
-    AdaptersConfig, ClippyAdapterConfig, ConfigError, EngineConfig, EngineMode, Level,
-    OutputConfig, OutputFormat, Policy, PublicApiErrorsConfig, RuleOptions, RuleSettings,
-    ScopeConfig, WorkspaceConfig,
-};
